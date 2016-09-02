1b687b6cbe3a042df32245d6e51f6afa4cf618ca
Danilo Barros <danilobjr@gmail.com>
2016-09-02 12:52:35 -0300

    showing data as a noob

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.todos = [
+            {
+                id: 1,
+                description: 'talk to John',
+                completed: false
+            },
+            { 
+                id: 2,
+                description: 'buy some fruits',
+                completed: true
+            }
+        ]
+    }
+
     render() {
         return (
-            <h1>Hello from a React Component.</h1>
+            <ul>
+                <li>{this.todos[0].description}</li>
+                {/* I use { ...  } to put some raw JS inner JSX */}
+                <li>{`I need to ${this.todos[1].description}`}</li>
+            </ul>
         );
     }
 }
