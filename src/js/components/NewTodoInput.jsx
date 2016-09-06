d7c8dfd0e6212830d697940b7b75fdf1ab505a2f
Danilo Barros <danilobjr@gmail.com>
2016-09-06 16:52:49 -0300

    handling onKeyUp event

--- src/js/components/NewTodoInput.jsx
+++ src/js/components/NewTodoInput.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { keyCodes } from 'utils'
 
 const NewTodoInput = props => 
-    <input className="new-todo" placeholder="What needs to be done?" />
+    <input className="new-todo" placeholder="What needs to be done?" onKeyUp={(e) => handleKeyUp(e, props)} />
+
+const handleKeyUp = (e, props) => {
+    const newDescription = e.target.value;
+
+    if (e.keyCode === keyCodes.ENTER) {
+        console.log(newDescription);
+    }
+}
 
 export {
     NewTodoInput
