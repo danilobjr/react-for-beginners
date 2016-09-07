26d5c22b32482f6bb0c353eb80ccffc3665c53e0
Danilo Barros <danilobjr@gmail.com>
2016-09-07 15:01:40 -0300

    hiding ClearCompletedButton only if none todo is completed

--- src/js/components/ClearCompletedButton.jsx
+++ src/js/components/ClearCompletedButton.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import classNames from 'classnames'
+
+const getCssClasses = props =>
+    classNames('clear-completed', { hidden: props.hidden })
 
 const ClearCompletedButton = props => 
-    <button className="clear-completed">Clear Completed</button>
+    <button className={getCssClasses(props)}>Clear Completed</button>
+
+ClearCompletedButton.propTypes = {
+    hidden: PropTypes.bool
+};
+
+ClearCompletedButton.defaultProps = {
+    hidden: false
+};
 
 export {
     ClearCompletedButton
--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -5,8 +5,11 @@ import { propTypes } from 'utils'
 const Footer = props => 
     <footer className="footer">
         <IncompleteTodosCount todos={props.todos} />
-        <ClearCompletedButton />
+        <ClearCompletedButton hidden={isClearCompletedButtonHidden(props)} />
     </footer>   
+
+const isClearCompletedButtonHidden = props =>
+    props.todos.every(todo => !todo.completed)
 
 Footer.propTypes = {
     todos: propTypes.todosRequired
