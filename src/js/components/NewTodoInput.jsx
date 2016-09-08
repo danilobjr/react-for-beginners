c5d678392de65cc43f91fb0480128746ebc9be87
Danilo Barros <danilobjr@gmail.com>
2016-09-08 12:40:33 -0300

    autofocus on NewTodoInput

--- src/js/components/NewTodoInput.jsx
+++ src/js/components/NewTodoInput.jsx
@@ -2,7 +2,12 @@ import React, { PropTypes } from 'react'
 import { keyCodes } from 'utils'
 
 const NewTodoInput = props => 
-    <input className="new-todo" placeholder="What needs to be done?" onKeyUp={(e) => handleKeyUp(e, props)} />
+    <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus
+        onKeyUp={(e) => handleKeyUp(e, props)} 
+    />
 
 const handleKeyUp = (e, props) => {
     const newDescription = e.target.value;
